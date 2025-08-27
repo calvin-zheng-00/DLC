ffbc4349d51f3350a99151d614b1eabb0b19a1af
calvin-zheng-00 <128666058+calvin-zheng-00@users.noreply.github.com>
2025-08-27 16:27:00 +1000

    Add files via upload

--- EEG/eeg_rawdata_extract.m
+++ EEG/eeg_rawdata_extract.m
@@ -1,40 +1,49 @@
 % close all;% clear all; clc;
 % Channel 2 is used as the reference node, so subtract 1 from all channel
 % numbers except channel 1.
-data = ALLEEG.data';
-Fs = ALLEEG.srate;
-loops = size(ALLEEG.event);
+
+% ALLEEG(1).event(4).type = 'S  2';
+
+data = ALLEEG(1).data';
+% Fs = ALLEEG.srate;
+loops = size(ALLEEG(1).event);
 loops = loops(2);
 events = [];
 sync = [];
-% main_event = readtable('C:\Users\czhe0008\Documents\EEG\sessions\22_05\events_bottle2.csv');
+% main_event = readtable('C:\Users\czhe0008\Documents\EEG\sessions\10_07\events_usb.csv');
 
 for i = 2:loops
-    temp = ALLEEG.event(i).type;
+    temp = ALLEEG(1).event(i).type;
     temp(1:2) = [];
     temp = str2double(temp);
     events = [events,temp];
-    temp2 = ALLEEG.event(i).latency;
+    temp2 = ALLEEG(1).event(i).latency;
     sync = [sync,temp2];
 end
 
-% If new setup
-audio_start = sync(events == 1);
-reach_start = audio_start(2:2:end);
-% start_trigger = find(events < 8);
-% reach_trigger = start_trigger(1:2:end);
-% reach_trigger = reach_trigger(91:120);
-% reach_trigger = reach_trigger(121:180);
+%% If new setup
+% audio_start = sync(events == 1);
+start_trigger = find(events == 1);
+% test_trigger = find(events == 2);
+% reach_trigger = start_trigger(423:452);
+reach_trigger = start_trigger(1:2:end);
+% reach_trigger = reach_trigger(1:30);
+% reach_trigger = reach_trigger(391:450);
 % reach_trigger = reach_trigger(2:2:end);
-% reach_start = sync(reach_trigger);
+reach_start = sync(reach_trigger);
 
 % grasp_trigger = start_trigger(2:2:end);
 % grasp_trigger = grasp_trigger(1:30);
 % grasp_trigger = grasp_trigger(31:90);
 % grasp_trigger = grasp_trigger(2:2:end);
 % grasp_start = sync(grasp_trigger);
+% start_trigger = find(events == 1);
+% test_trigger = start_trigger(181:240);
+% test_trigger = test_trigger(2:2:end);
+% test_start = sync(test_trigger);
 
-% start_trigger = find(events < 12);
+% If old setup
+% start_trigger = find(events == 7);
 % diff_trigger = diff(start_trigger);
 % trigger_idx = find(diff_trigger ~= 1) + 1;
 % sync_idx = start_trigger([1,trigger_idx]);
@@ -53,14 +62,21 @@ reach_start = audio_start(2:2:end);
 % reach_start = sync(sync_idx)+76*25; % Bottle 68, usb 76
 % grasp_start = sync(sync_idx)+105*25; % Bottle 101, usb 105
 
-for i = 1:length(reach_start)
-    data_points = (reach_start(i)-2499):(reach_start(i)+2000);
-    save_flag = data(data_points,1:31);
-    save_folder = 'C:\Users\czhe0008\Documents\EEG\raw_data\test\Right_Pinch_Release\';
+for k = 1:31
+    save_flag = [];
+    for i = 1:length(reach_start)
+        data_points = (reach_start(i)-999):(reach_start(i)+2000);
+        if (reach_start(i)+2000)>size(data,1)
+            continue
+        end
+        save_flag = [save_flag,data(data_points,k)];
+        
+    end
+    save_folder = 'C:\Users\czhe0008\Documents\EEG\raw_data\test2\per_channel\';
     if not(isfolder(save_folder))
         mkdir(save_folder)
     end
-    save_loc = strcat(save_folder,'June_30_right_pinch_release_trial',int2str(i),'.mat');
+    save_loc = strcat(save_folder,'channel',int2str(k),'.mat');
 
     save(save_loc, "save_flag")
 end
---
+++ EEG/eeglab_data_edit.m
@@ -0,0 +1,72 @@
+% close all; clear all; clc;
+% Channel 2 is used as the reference node, so subtract 1 from all channel
+% numbers except channel 1.
+
+data_set = 2;
+loops_event = size(ALLEEG(data_set).event);
+loops_event = loops_event(2);
+
+second_check = 0;
+for i = 2:loops_event
+    if ALLEEG(data_set).event(i).code == "Stimulus"
+        % Assuming stimulus come in pairs
+        if second_check == 0
+            second_check = 1;
+        else
+            second_check = 0;
+            ALLEEG(data_set).event(i).type = 'S  2';
+            ALLEEG(data_set).urevent(i).type = 'S  2';
+        end
+    end
+end
+
+count_events = 0;
+for i = 2:loops_event
+    if ALLEEG(data_set).event(i).type == 'S  1'
+        if (count_events >=30) && (count_events < 60)
+            ALLEEG(data_set).event(i).type = 'S  3';
+            count_events = count_events + 1;
+        elseif count_events > 60
+            ALLEEG(data_set).event(i).type = 'S  4';
+            count_events = count_events + 1;
+        else
+            count_events = count_events + 1;
+        end
+    end
+end
+
+events = [];
+sync = [];
+for i = 2:loops_event
+    temp = ALLEEG(data_set).event(i).type;
+    temp(1:2) = [];
+    temp = str2double(temp);
+    events = [events,temp];
+    temp2 = ALLEEG(data_set).event(i).latency;
+    sync = [sync,temp2];
+end
+
+%% If new setup
+data = ALLEEG(data_set).data';
+reach_trigger = find(events == 1);
+return_trigger = find(events == 2);
+reach_start = sync(reach_trigger);
+
+for k = 1:31
+    save_flag = [];
+    for i = 1:length(reach_start)
+        data_points = (reach_start(i)-999):(reach_start(i)+2000);
+        if (reach_start(i)+2000)>size(data,1)
+            continue
+        end
+        save_flag = [save_flag,data(data_points,k)];
+        
+    end
+    save_folder = 'C:\Users\czhe0008\Documents\EEG\raw_data\test2\per_channel\';
+    if not(isfolder(save_folder))
+        mkdir(save_folder)
+    end
+    save_loc = strcat(save_folder,'channel',int2str(k),'.mat');
+
+    save(save_loc, "save_flag")
+end
